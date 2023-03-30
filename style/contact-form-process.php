bc21d8ea1cdfd0f3a065684c0518c3cf221e9b2a
Brayden039 <113641758+Brayden039@users.noreply.github.com>
2023-03-29 21:37:53 -0400

    contact-form changes

--- style/contact-form-process.php
+++ style/contact-form-process.php
@@ -2,8 +2,8 @@
 if (isset($_POST['Email'])) {
 
     // EDIT THE 2 LINES BELOW AS REQUIRED
-    $email_to = "you@yourdomain.com";
-    $email_subject = "New form submissions";
+    $email_to = "alphaonps2@gmail.com";
+    $email_subject = "Brayden's Website Form Submission";
 
     function problem($error)
     {
